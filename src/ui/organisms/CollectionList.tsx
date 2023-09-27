659109d5f4209c66e71d083e638771504d721c64
Przemyslaw Leksa <przemyslaw.leksa@kitopi.com>
2023-09-27 16:10:11 +0200

    heading

--- src/ui/organisms/CollectionList.tsx
+++ src/ui/organisms/CollectionList.tsx
@@ -15,9 +15,12 @@ export const CollectionList = ({ collection, pageNumber }: CollectionListProps) 
 
 	return (
 		<div>
-			<div className="mb-6" role="heading">
-				<h1 className="text-3xl">{collection[0].name}</h1>
-				<div>{collection[0].description}</div>
+			<div className="mb-6">
+				<h1 className="text-3xl" role="heading">
+					{collection[0].name}
+					{collection[0].description}
+				</h1>
+				<div></div>
 			</div>
 
 			<ProductList products={collection[0].products} currentPage={Number(pageNumber)} />
