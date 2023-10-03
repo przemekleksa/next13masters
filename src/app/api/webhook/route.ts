5b719e7e8d7a67d85d6f829474bd03007d54be13
Przemyslaw Leksa <przemyslaw.leksa@kitopi.com>
2023-10-03 21:08:35 +0200

    4.8

---
+++ src/app/api/webhook/route.ts
@@ -0,0 +1,20 @@
+import { revalidatePath } from "next/cache";
+import { NextResponse, type NextRequest } from "next/server";
+
+export async function POST(request: NextRequest): Promise<Response> {
+	const json: unknown = await request.json();
+	if (
+		typeof json === "object" &&
+		json &&
+		"productId" in json &&
+		typeof json.productId === "string"
+	) {
+		// json.productId;
+		console.log("Revalidating", `/product/${json.productId}`);
+		revalidatePath(`/product/${json.productId}`);
+		revalidatePath(`/products`);
+		return NextResponse.json({}, { status: 201 });
+	}
+
+	return NextResponse.json({ message: "Invalid body" }, { status: 400 });
+}
