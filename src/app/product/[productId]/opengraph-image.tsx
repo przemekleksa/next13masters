1d1bb946f22775073f5a5edadc0be9a39218b967
Przemyslaw Leksa <przemyslaw.leksa@kitopi.com>
2023-10-07 16:57:34 +0200

    2

---
+++ src/app/product/[productId]/opengraph-image.tsx
@@ -0,0 +1,62 @@
+import { ImageResponse } from "next/server";
+import { notFound } from "next/navigation";
+import { getProductDetailsById } from "@/api/products";
+
+export const runtime = "edge";
+
+export const contentType = "image/png";
+export const size = {
+	width: 1200,
+	height: 720,
+};
+export const alt = "Open Graph Image";
+
+interface OpenGraphImageProps {
+	params: { productId: string };
+	// searchParams?: { [key: string]: string | string[] };
+}
+
+export default async function OpenGraphImage({ params }: OpenGraphImageProps) {
+	const product = await getProductDetailsById(params.productId);
+
+	if (!product) {
+		notFound();
+	}
+
+	return new ImageResponse(
+		(
+			<div
+				style={{
+					width: "100%",
+					height: "100%",
+					background: "white",
+					alignItems: "center",
+					display: "flex",
+					justifyContent: "center",
+				}}
+			>
+				<img
+					src={product?.images[0]?.url || ""}
+					alt={product?.name || ""}
+					height={50}
+					style={{
+						marginRight: 20,
+					}}
+				/>
+				<div
+					style={{
+						display: "flex",
+						flexDirection: "column",
+						gap: 0,
+					}}
+				>
+					<h2 style={{ margin: 0 }}>{product.name}</h2>
+					<p>{product.description}</p>
+				</div>
+				<div style={{ display: "flex", alignItems: "flex-end" }}>
+					<p>{product.categories[0]?.name}</p>
+				</div>
+			</div>
+		),
+	);
+}
